df9c9a097169c79ae4dd020180082938339b778c
Akz2 <128789113+Akz2@users.noreply.github.com>
2025-02-24 21:54:06 +0530

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeItem, updateQuantity } from '../store/CartSlice.js';
+import { removeItem, updateQuantity } from '../store/CartSlice.jsx';
 
 const CartItem = ({ handleContinueShopping }) => {
   const dispatch = useDispatch();
